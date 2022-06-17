a34d5d10a151e54bcbb10b40a47c35cfa17a709a
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2022-06-17 12:25:40 +0100

    last

--- index.js
+++ index.js
@@ -1,53 +1,72 @@
 // - Create an alert that lists all users, with their ids, names and what city they're from ✅
+function listUsers() {
+  let greeting = ''
+
+  for (let user of users) {
+    // getting the name
+    let name = user.name
+    // getting the id
+    let id = user.id
+    // getting the city
+    let city = user.address.city
+
+    greeting += `Id: ${id} Name: ${name} City: ${city}\n\n`
+  }
+
+  alert(greeting)
+}
+
 // - Prompt the user for a user id and then: ✅
-// - Display an alert with the username and all the todos titles that belong to that user
+function getUserId() {
+  let userId = Number(prompt('Enter your user id'))
+  return userId
+}
+
 //    - to find the user with this id ✅
+function findUser() {
+  // find the user with this id
+  let theUser = null
+
+  for (let user of users) {
+    if (user.id === userId) {
+      theUser = user
+      break
+    }
+  }
+
+  return theUser
+}
+
 //    - to find the todos with this user id ✅
-//   -  alert the username and the todo titles
+function findTodos() {
+  // // find the todos with this user id
+  let theTodos = []
 
-let greeting = ''
-
-for (let user of users) {
-  // getting the name
-  let name = user.name
-  // getting the id
-  let id = user.id
-  // getting the city
-  let city = user.address.city
-
-  greeting += `Id: ${id} Name: ${name} City: ${city}\n\n`
-}
-
-alert(greeting)
-
-let userId = Number(prompt('Enter your user id'))
-
-// find the user with this id
-let theUser = null
-
-for (let user of users) {
-  if (user.id === userId) {
-    theUser = user
-    break
+  for (let todo of todos) {
+    if (todo.userId === userId) {
+      theTodos.push(todo)
+    }
   }
+
+  return theTodos
 }
 
-// // find the todos with this user id
-let theTodos = []
+//   -  alert the username and the todo titles ✅
+function showUserTodos() {
+  // alert the username and the todo titles
+  let finalMessage = ''
 
-for (let todo of todos) {
-  if (todo.userId === userId) {
-    theTodos.push(todo)
+  finalMessage += `Hello ${user.username}:\n\n`
+
+  for (let todo of userTodos) {
+    finalMessage += `- ${todo.title}\n`
   }
+
+  alert(finalMessage)
 }
 
-// alert the username and the todo titles
-let finalMessage = ''
-
-finalMessage += `Hello ${theUser.username}:\n\n`
-
-for (let todo of theTodos) {
-  finalMessage += `- ${todo.title}\n`
-}
-
-alert(finalMessage)
+listUsers()
+let userId = getUserId()
+let user = findUser()
+let userTodos = findTodos()
+showUserTodos()
